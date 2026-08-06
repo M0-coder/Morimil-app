cd13036632c1ff55fb1bca2e4c0f06f84588f55c
morimilpabfelon-cell <morimil.pabfelon@gmail.com>
2026-08-06 06:54:34 -0700

    [QA-6] Enable dependency locking bootstrap

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
     id("com.google.devtools.ksp") version "2.3.2" apply false
 }
+
+allprojects {
+    dependencyLocking {
+        lockAllConfigurations()
+    }
+}
